ffd07d82ee4c796d15f44a98bf83cbd44619826f
JakeOJeff <realjakeojeff@gmail.com>
2025-08-27 04:00:42 +0530

    Added a readmore button with animations

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -26,17 +26,23 @@ export default function BlogPage() {
       <p className=" text-lg font-mono ml-10 mt-10">~{pathname}</p>
 
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
-        {posts.map((post) => (
-          <Link
-            key={post.slug}
-            href={`/blog/${post.slug}`}
-            className="bg-white p-5 rounded-xl border border-gray-300 shadow-md hover:shadow-lg transition block duration-500"
-          >
-            <h2 className="text-xl font-semibold text-gray-800">{post.title}</h2>
-            <p className="text-sm text-gray-500 mt-1">{post.description}</p>
-            <div className="mt-2 text-sm text-gray-400">{post.date}</div>
-          </Link>
-        ))}
+        {posts.map((post) => {
+          return (
+            <Link
+              key={post.slug}
+              href={`/blog/${post.slug}`}
+              className="group bg-white p-5 rounded-xl border border-gray-300 shadow-md hover:shadow-lg transition block duration-500"
+            >
+              <h2 className="text-xl font-semibold text-gray-800">{post.title}</h2>
+              <p className="text-sm text-gray-500 mt-1">{post.description}</p>
+              <div className="flex justify-between items-center">
+                <div className="mt-2 text-sm text-gray-400">{post.date}</div>
+                <p className=" text-sky-600 transition duration-300">Read More &#62&#62<span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span></p>
+              </div>
+
+            </Link>
+          );
+        })}
       </div>
     </main>
   );
